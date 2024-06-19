0af35ac61fe99e1f28a65b8177aef6b7935127ba
Jinjiro-San <jinjirosan@users.noreply.github.com>
2024-06-19 22:58:20 +0200

    v2-2.5 (stable) - refactor C1.1.0

--- icons.py
+++ icons.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
+#    ___     _                                         
+#   / _ \___| |_ ___ _ __                              
+#  / /_)/ _ \ __/ _ \ '__|                             
+# / ___/  __/ ||  __/ |                                
+# \/    \___|\__\___|_|                                                                                 
+#    ___ _             _   __    __      _       _     
+#   / _ \ | __ _ _ __ | |_/ / /\ \ \__ _| |_ ___| |__  
+#  / /_)/ |/ _` | '_ \| __\ \/  \/ / _` | __/ __| '_ \ 
+# / ___/| | (_| | | | | |_ \  /\  / (_| | || (__| | | |
+# \/    |_|\__,_|_| |_|\__| \/  \/ \__,_|\__\___|_| |_|
+#                       .: auto-grow the greens yo :.                          
+#
+# Automated plant monitoring and watering system
+#
+# hardware platform  : Raspberry Pi Zero W
+# HAT                : Pimoroni Grow Hat Mini
+# Water drivers      : COM3700 Mini submersible water pump
+# Sensors            : Capacitive Soil moisture sensor with PFM output
+#                    : BME280 Temperature, Humidity, Air pressure
+#                    : LTR-559 light and proximity sensor 
+# Codebase           : Python3
+#
+# (2024) JinjiroSan
+#
+# PeterPlantwatch/
+# ├── main.py
+# ├── config.py
+# ├── views.py
+# ├── controllers.py
+# ├── models.py
+# ├── icons.py
+# ├── constants.py
+# ├── hardware.py
+# └── plant_logging.py
+#
+# icons.py : v2-2.5 (stable) - refactor C1.0.0
 
 from PIL import Image
 
