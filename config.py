55bf04c06c785b4f977117f7ae0f4efd034f60fd
Jinjiro-San <jinjirosan@users.noreply.github.com>
2024-06-19 11:56:42 +0200

    v2.1-refactor 0.0.2

--- config.py
+++ config.py
@@ -1,46 +1,72 @@
 #!/usr/bin/env python3
 
-import yaml
-import pathlib
-import sys
-
 class Config:
     def __init__(self):
-        self.config = {}
+        self.config = None
         self._last_save = ""
 
+        self.channel_settings = [
+            "enabled",
+            "warn_level",
+            "wet_point",
+            "dry_point",
+            "watering_delay",
+            "auto_water",
+            "pump_time",
+            "pump_speed",
+            "water_level",
+        ]
+
+        self.general_settings = [
+            "alarm_enable",
+            "alarm_interval",
+        ]
+
     def load(self, settings_file="settings.yml"):
-        settings_file = pathlib.Path(settings_file if len(sys.argv) <= 1 else sys.argv[1])
+        if len(sys.argv) > 1:
+            settings_file = sys.argv[1]
+
+        settings_file = pathlib.Path(settings_file)
+
         if settings_file.is_file():
             try:
-                with open(settings_file, "r") as file:
-                    self.config = yaml.safe_load(file)
+                self.config = yaml.safe_load(open(settings_file))
             except yaml.parser.ParserError as e:
-                logging.error(f"Error parsing settings file {settings_file}: {e}")
-                raise
+                raise yaml.parser.ParserError(
+                    f"Error parsing settings file: {settings_file} ({e})"
+                )
 
     def save(self, settings_file="settings.yml"):
-        settings_file = pathlib.Path(settings_file if len(sys.argv) <= 1 else sys.argv[1])
+        if len(sys.argv) > 1:
+            settings_file = sys.argv[1]
+
+        settings_file = pathlib.Path(settings_file)
+
         dump = yaml.dump(self.config)
-        if dump != self._last_save:
+
+        if dump == self._last_save:
+            return
+
+        if settings_file.is_file():
             with open(settings_file, "w") as file:
                 file.write(dump)
-            self._last_save = dump
+
+        self._last_save = dump
 
     def get_channel(self, channel_id):
         return self.config.get(f"channel{channel_id}", {})
 
     def set(self, section, settings):
-        if not isinstance(settings, dict):
+        if isinstance(settings, dict):
+            self.config[section].update(settings)
+        else:
             raise ValueError("Settings should be a dictionary")
-        self.config.setdefault(section, {}).update(settings)
 
-    def set_channel(self, channel_id, settings):
-        self.set(f"channel{channel_id}", settings)
+    def set_channel(self, channel_id, channel):
+        self.set(f"channel{channel_id}", channel.to_dict())
 
     def get_general(self):
         return self.config.get("general", {})
 
     def set_general(self, settings):
         self.set("general", settings)
-
