d03e70df0d5c00c4dc4926ccc55f126c25087539
Jinjiro-San <jinjirosan@users.noreply.github.com>
2024-06-19 12:39:40 +0200

    v2.1-refactor 0.0.1

--- plant_logging.py
+++ plant_logging.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-# plant_logging.py
 import logging
 import os
 
@@ -28,11 +27,15 @@ def setup_channel_logger(channel_id):
 # Setting up loggers for each channel
 channel_loggers = {i: setup_channel_logger(i) for i in range(1, 4)}
 
-def log_values(channel_id, soil_moisture_abs, soil_moisture_percent, water_given, light_level):
+def log_values(channel_id, soil_moisture_abs, soil_moisture_percent, water_given, light_level, wet_point, dry_point, auto_water):
     logger = channel_loggers[channel_id]
     water_status = "Yes" if water_given else "No"
+    auto_water_status = "Enabled" if auto_water else "Disabled"
     message = (f"soil moisture (abs): {soil_moisture_abs}, "
                f"soil moisture (%): {soil_moisture_percent:.2f}, "
                f"water given: {water_status}, "
-               f"light level: {light_level}")
+               f"light level: {light_level}, "
+               f"wet point: {wet_point}, "
+               f"dry point: {dry_point}, "
+               f"auto water: {auto_water_status}")
     logger.info(message)
